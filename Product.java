4dea84b3c360753ad1d6e8e7e6b9111fe6e2f65e
RalpA-AFK <ralpaa@farmingdale.edu>
2025-05-14 15:31:58 -0400

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,16 @@
+public abstract class Product {
+    String name;
+    double price;
+    String category;
+
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + category + ") - $" + String.format("%.2f", price);
+    }
+}
